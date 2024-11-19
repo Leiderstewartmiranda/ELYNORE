cef9051653e43d047bcb545e89d3d8be996d78fa
Dahi montoya <169170259+dahianam16@users.noreply.github.com>
2024-11-18 20:42:47 -0500

    actualización rutas

--- perfumes/src/nav.jsx
+++ perfumes/src/nav.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './proyec.css'
 import { CardList } from './datos';
+import { CardListM } from './datosF';
+import { CardListH } from './datosH';
+import { CardListU } from './datosU';
 
 export function Nav() {
     return (
@@ -16,6 +19,9 @@ export function Nav() {
           <Routes>
             <Route path="/"element={<CardList />} />
             <Route path="/Mujeres"element={<CardListM />} />
+            <Route path="/Hombres"element={<CardListH />} />
+            <Route path="/Unisex"element={<CardListU />} />
+            
           </Routes>
         
         </Router>
