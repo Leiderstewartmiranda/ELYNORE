2bfb08b0d688044ee68e780179a31d8740ff55b2
Esteban <128088560+EstebanHenaovasquez@users.noreply.github.com>
2024-11-18 20:30:04 -0500

    Cartas inicio

--- perfumes/src/Cards.jsx
+++ perfumes/src/Cards.jsx
@@ -2,13 +2,20 @@ import React, {useState} from 'react'
 import './proyec.css'
 
 const Card = ({card}) => {
+    const [botonActivo, setBotonActivo] = useState(false);
+    const handleClick = () => {setBotonActivo(!botonActivo);
+    };
+
    
     return(
         
             <div className="card">
                 <img src={card.imagen} alt={card.titulo} />
                 <h3>{card.nombre}</h3>
-                <p>{card.precio}</p>
+                <div className="boton">
+                    <p>${card.precio}</p>
+                    <button className={botonActivo ? 'boton-elimi' : 'boton-agg'} onClick={handleClick}>{botonActivo ? 'ELIMINAR' : 'AGREGAR'}</button>
+                </div>
               
             </div>
     
